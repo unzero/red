608d0f63076614c4bc17d782a43c17818a351ea3
Ciro Garcia <ciroivan94@gmail.com>
2024-08-19 21:31:18 -0600

    refactor: move render_template and redirect to new module in src/web/common.rs.

---
+++ src/web/common.rs
@@ -0,0 +1,22 @@
+use actix_web::HttpResponse;
+use tera::Context;
+
+pub fn render_template( 
+    template_name: &str, 
+    context: &Context, 
+    templates: actix_web::web::Data<tera::Tera> 
+) -> HttpResponse {
+    let template = templates.render(template_name, context).expect("Error");
+    HttpResponse::Ok()
+    .content_type("text/html; charset=utf-8")
+    .body(template)
+
+}
+
+pub fn redirect(
+    location: &str
+) -> HttpResponse {
+    HttpResponse::Found()
+    .insert_header(( actix_web::http::header::LOCATION, location, ))
+    .body("ok")
+}
