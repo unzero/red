6a1d4479caf2cdee9ebbbcd211c6a90fb1696336
Ciro Garcia <ciroivan94@gmail.com>
2024-08-19 21:35:43 -0600

    refactor: update SessionError in src/web/session.rs.

--- src/web/session.rs
+++ src/web/session.rs
@@ -2,8 +2,10 @@ use actix_identity::Identity;
 
 use super::errors::RedHttpError;
 
-pub fn validate_session(identity: Option<Identity>) -> Result<String, RedHttpError> {
-    let uuid = identity.ok_or_else(|| RedHttpError::session_error() )?
-        .id().map_err(|_e| RedHttpError::session_error() )?;
+pub fn validate_session(
+    identity: Option<Identity>
+) -> Result<String, RedHttpError> {
+    let uuid = identity.ok_or_else(|| RedHttpError::SessionError )?
+        .id().map_err(|_e| RedHttpError::SessionError )?;
     Ok(uuid)
 }
