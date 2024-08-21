52a2ed832d4dd44dba016e1ae7160f4c76375edc
Ciro Garcia <ciroivan94@gmail.com>
2024-08-20 22:05:35 -0600

    feat: add default_service to routes in src/web/ mod.rs.

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -14,38 +14,45 @@ pub use common::not_found;
 pub fn get_configuration(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::resource("/")
-            .route(web::get().to(index)),
+            .route(web::get().to(index))
+            .default_service( actix_web::web::route().to( not_found ) ),
     );
 
     cfg.service(
         web::resource("/red")
             .route(web::post().to(red_login))
-            .route(web::get().to(home)),
+            .route(web::get().to(home))
+            .default_service( actix_web::web::route().to( not_found ) ),
     );
 
     cfg.service(
         web::resource("/logout")
-            .route(web::post().to(red_logout)),
+            .route(web::post().to(red_logout))
+            .default_service( actix_web::web::route().to( not_found ) )
     );
 
     cfg.service(
         web::resource("/open_file")
-            .route(web::post().to(open_file)),
+            .route(web::post().to(open_file))
+            .default_service( actix_web::web::route().to( not_found ) ),
     );
 
     cfg.service(
         web::resource("/change_directory")
-            .route(web::post().to(change_directory)),
+            .route(web::post().to(change_directory))
+            .default_service( actix_web::web::route().to( not_found ) ),
     );
 
     cfg.service(
         web::resource("/new_file")
-            .route(web::post().to(new_file)),
+            .route(web::post().to(new_file))
+            .default_service( actix_web::web::route().to( not_found ) ),
     );
 
     cfg.service(
         web::resource("/save_file")
-            .route(web::post().to(save_file)),
+            .route(web::post().to(save_file))
+            .default_service( actix_web::web::route().to( not_found ) ),
     );
 }
 
